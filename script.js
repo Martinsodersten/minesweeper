49d8e67d6216472614732c7d3f81003f2d639eb1
Martinsodersten <martin_sodersten@hotmail.com>
2024-11-04 21:16:49 +0100

    Mines left

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 // Display page / UI
+// 1. Make a board with tiles/mines
 
 import { createBoard } from "./minesweeper.js"
 
 const BOARD_SIZE = 10
-const NUMBER_OF_MINES = 5
+const NUMBER_OF_MINES = 10
+
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
 const boardElement = document.querySelector('.board');
-boardElement.style.setProperty("--size", BOARD_SIZE)
+const mineLeftText = document.querySelector("[data-mine-count]");
+
+
+
 board.forEach(row => {
     row.forEach(tile => {
         boardElement.append(tile.element)
     })
 })
+boardElement.style.setProperty("--size", BOARD_SIZE)
+mineLeftText.textContent = NUMBER_OF_MINES
 
-// 1. Make a board with tiles/mines
 // 2. left click on tiles / reveal tiles
 // 3. Right click on tiles / mark tiles
 // 4. Check or win/lose
