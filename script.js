f1857c3e4ce339bc119118a30f79bbfd8b2e0f5e
Martinsodersten <martin_sodersten@hotmail.com>
2024-11-04 21:12:04 +0100

    Make a board with tiles/mines

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-// Display page
+// Display page / UI
+
+import { createBoard } from "./minesweeper.js"
+
+const BOARD_SIZE = 10
+const NUMBER_OF_MINES = 5
+
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
+const boardElement = document.querySelector('.board');
+boardElement.style.setProperty("--size", BOARD_SIZE)
+board.forEach(row => {
+    row.forEach(tile => {
+        boardElement.append(tile.element)
+    })
+})
 
 // 1. Make a board with tiles/mines
 // 2. left click on tiles / reveal tiles
